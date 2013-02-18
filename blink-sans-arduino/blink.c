dbd74f05356ab32b015a19e0ae34c1471cdea34b
Jean Schurger <jean@schurger.org>
2013-02-18 15:04:59 -0500

    First commit.

---
+++ blink-sans-arduino/blink.c
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BLINK_DELAY_MS 500
+
+int main (void) {
+  /* set pin 5 of PORTB for output*/
+  DDRB |= _BV(DDB5);
+  for (;;) {
+    /* set pin 5 high to turn led on */
+    PORTB |= _BV(PORTB5);
+    _delay_ms(BLINK_DELAY_MS);
+    /* set pin 5 low to turn led off */
+    PORTB &= ~_BV(PORTB5);
+    _delay_ms(BLINK_DELAY_MS);
+  }
+  return 0;
+}
